4f2ec905436f17c1b60f5eeaec261ba4c57f9c9e
Pramod Jain <pramod.aug17@gmail.com>
2021-05-24 01:21:47 +0530

    New api :scanQR, submitTrans, schedule, unschedule

--- homedairy-web/routes/restapi.js
+++ homedairy-web/routes/restapi.js
@@ -7,23 +7,41 @@ var CustomerAction = require("../actions/customers");
 var CRMAction = require("../actions/crm");
 
 /* POST Methods */
-/* User login and sign up */
+/* Let user login */
 router.post("/dologin", AuthorizationAction.handler.login);
+/* let refresh token of already logged in user */
 router.post("/refreshtoken", AuthorizationAction.handler.refreshToken);
 
+/* User will verify its single existence */
 router.post("/verifyUser", UserAction.handler.verify);
+/* Let the customer or the consumer signup */
 router.post("/customer/signup", UserAction.handler.addCustomer);
+/* let the seller sign up to serve */
 router.post("/seller/signup", UserAction.handler.addSeller);
-
+/* let the consumer subscirbe to the producer */
 router.post("/subscribe", CRMAction.handler.subscribe);
+/* let two consumer link thier account togetheer */ // TODO: to be implemented
 router.post("/linkAccount", CRMAction.handler.linkAccount);
+/* */ // TODO: I dont know what it is  to be implemented
 router.post("/linkAccount/:response", CRMAction.handler.linkResponse);
+/* let user submit a transaction happened today */
+router.post("/submitTransaction", CustomerAction.handler.makeEntry);
+/* let user make schedule a product */
+router.post("/scheduleIt", CustomerAction.handler.scheduleIt);
+/* let user make unschedule a product */
+router.post("/unscheduleIt", CustomerAction.handler.unScheduleIt);
 
 /* GET Methods */
+/* Let user search for other user but opposite type */
 router.get("/get/users", UserAction.handler.getUser);
+/* let user fetch details of one user */
 router.get("/get/users/:userid", UserAction.handler.getUser);
+/* let user fetch schedules */
 router.get("/get/schedules", CustomerAction.handler.fetchSchedules);
+/* let user fetch their passbook of enteries */
 router.get("/get/passbook", CustomerAction.handler.fetchPassbook);
-router.get("scan/:qrCode", CustomerAction.handler.scanQR);
+/* let user get it todays entried by scanning QR */
+router.get("/scan/:qrCode", CustomerAction.handler.scanQR);
+
 
 module.exports = router;
