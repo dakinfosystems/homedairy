9157eb56a65876ce9b0cde998adaf0cf865f38fa
Pramod Jain <pramod.aug17@gmail.com>
2021-05-24 01:19:07 +0530

    passbook feature update

--- homedairy-web/models/passbook/helper.js
+++ homedairy-web/models/passbook/helper.js
@@ -1,46 +1,58 @@
 var QueryBuilder = require("../../lib/queryBuilder");
 var UserConfig = require("../../configs/user.config").user
 
-var scheduleProcedure = {
+/**
+ * 
+ */
+var TransactionTbl = {
     "id": "id",
-    "customerId": "cust_id",
-    "sellerName": "seller_name",
-    "occurrence": "occurrence",
-    "productName": "product_name",
-    "productQuantity": "product_quantity"
+    "scheduleId": "schedule_id",
+    "custId": "cust_id",
+    "sellerId": "seller_id",
+    "productId": "product_id",
+    "productQuantity": "quantity",
+    "time": "time",
+    "productPrice": "price"
 };
 
-function getScheduleDefaultValue() {
+/**
+ * This function return default value of Table
+ * 
+ * @param {string} col column name of transaction table in DB
+ * @returns default value of given colums
+ */
+function getTransactionDefaultValue(col) {
     let value = "";
 
-    console.log(col)
+    // console.log(col)
     switch (col) {
-        case "name":
+        case "schedule_id":
             break;
         default:
             break;
     }
+    return value;
 }
 
-scheduleProcedure.fn = {
-    name: () => { return ""; },
-    getDefaultValue: getScheduleDefaultValue
+TransactionTbl.fn = {
+    name: () => { return "Transcation_Tbl"; },
+    getDefaultValue: getTransactionDefaultValue
 }
 
 exports.whereToString = (whereArr) => {
     return QueryBuilder.whereToString(whereArr);
 }
 
-exports.subsTable = {
+exports.transactionTable = {
     fromDBtoParam : (dbRow, extend) => {
-        return QueryBuilder.fromDBtoParam(dbRow, extend, scheduleProcedure);
+        return QueryBuilder.fromDBtoParam(dbRow, extend, TransactionTbl);
     },
 
     fromParamtoDB: (paramRow, extend) => {
-        return QueryBuilder.fromParamtoDB(paramRow, extend, scheduleProcedure);
+        return QueryBuilder.fromParamtoDB(paramRow, extend, TransactionTbl);
     },
 
     constructWhere(conditions, alias, extend) {
-        return QueryBuilder.getWhereCondition(conditions, alias, extend, scheduleProcedure);
+        return QueryBuilder.getWhereCondition(conditions, alias, extend, TransactionTbl);
     }
 };
--- homedairy-web/models/passbook/index.js
+++ homedairy-web/models/passbook/index.js
@@ -1,25 +1,27 @@
 let HelperFn = require("./helper");
 let CommonHepler = require("../../lib/common.helper");
 let pool = require("../../lib/mysql").pool;
-let NativeDate = require("../../lib/native/date");
+let DateUtil = require("../../lib/native/date")
+
+let globalValue = 0;
 
 exports.list = (id, pageNo) => {
     // console.log("UserModel list: " + JSON.stringify(where));
     return new Promise((resolve, reject) => {
-        var headers = [];
-        var data = [];
-        var connection;
+        let headers = [];
+        let data = [];
+        let connection;
 
         pool.getSession().then(session => {
             let limit = 10;
             connection = session;
             // console.log("userModel list where: " + whereInArr);
-            let collection = session.getSchema(process.env.DB_NAME).getCollection("Transaction_Tbl");
+            let table = session.getSchema(process.env.DB_NAME).gettable("Transaction_Tbl");
 
             pageNo = pageNo * limit;
-            collection = collection.find("_id=:userid").bind("userid", id).offset(pageNo).limit(limit);
+            table = table.find("_id=:userid").bind("userid", id).offset(pageNo).limit(limit);
 
-            return collection.execute(results => {
+            return table.execute(results => {
                 // console.log("User Secret data: " + JSON.stringify(data, null, 4));
                 console.log("Passbook query: " + JSON.stringify(results, null, 4));
                 data.push(results);
@@ -28,10 +30,10 @@ exports.list = (id, pageNo) => {
             });
         }).then(() => {
             /** process data */
-            var rawresult = CommonHepler.constructResults(headers, data);
-            var results = [];
-            for(var index in rawresult) {
-                var result = HelperFn.subsTable.fromDBtoParam(rawresult[index]);
+            let rawresult = CommonHepler.constructResults(headers, data);
+            let results = [];
+            for(let index in rawresult) {
+                let result = HelperFn.transactionTable.fromDBtoParam(rawresult[index]);
                 results.push(
                     result
                 );
@@ -47,42 +49,112 @@ exports.list = (id, pageNo) => {
 }
 
 exports.save= (data) => {
+    // console.log("Passbook data: " +JSON.stringify(data));
     return new Promise((resolve, reject) => {
         let connection;
-        let now = new Date();
+        let millicount = 0;
 
-        pool.getSession((session) => {
-            let millitimestr = "0000000000000000";
-            let entry = {};
-            let collection = session.getSchema(process.env.DB_NAME).getCollection("Transaction_Tbl");
+        pool.getSession().then((session) => {
+            let insertCalled = false;
 
             connection = session;
-            millitimestr += now.getTime()/process.env.DIVIDEND;
-            entry["_id"] = "TRANSAC" + millitimestr.slice(-16);
 
-            for(key of [
-                "toId",
-                "to",
-                "fromId",
-                "from",
-                "description",
-                "time",
-                "toType",
-                "fromType"
-            ]) {
-                if(data.hasOwnProperty(key)) {
-                    entry[key] = data[key]
+            let table = session.getSchema(process.env.DB_NAME).getTable("Transaction_Tbl");
+            for(let index in data) {
+                // let [col, values] = {};
+                let millitimestr = "0000000000000000" + DateUtil.getEpochStr();
+                
+                /* Looking for entry which has similar time */
+                if(globalValue === millitimestr) {
+                    millicount += 1;
+                } else {
+                    millicount = 0;
                 }
+                
+                data[index]["id"] = "TRANSAC" + millitimestr.slice(-16) + ("000"+millicount).slice(-3);
+                globalValue = millitimestr;
+
+                data[index]["productQuantity"] = parseInt(data[index]["productQuantity"]);
+
+                let [col, values] = CommonHepler.getTableInsert(data[index], HelperFn.transactionTable)
+                // console.log("Passbook columns: " +col+ "\nvalues: " +values);
+                
+                if(!insertCalled) {
+                    table = table.insert(col);
+                    insertCalled = true;
+                }
+
+                table = table.values(values);
             }
 
-            collection.add(entry);
-            return collection.execute();
+            return table.execute();
         }).then((result) => {
             resolve(result);
         }).catch((err) => {
-            reject(err);
+            console.error("Passbook save err:" +JSON.stringify(err));
+            if(!err.info) {
+                err.info = {};
+                err.info.code = "FF";
+            }
+            reject(err)
         }).finally(() => {
             connection.close();
         });
     })
+}
+
+/**
+ * This function retrieves todays entries made by user
+ * 
+ * @param {*} customerId 
+ * @param {*} sellerid Optional
+ * @returns It returns a promise object 
+ */
+exports.getTodaysTransactionOf = (customerId, sellerid) => {
+    return new Promise((resolve, reject) => {
+        let connection;
+        let headers = [];
+        let data = [];
+
+        pool.getSession().then((session) => {
+            connection = session;
+            let transactionTime = new Date();
+            let table = session.getSchema(process.env.DB_NAME).getTable("Transaction_Tbl");
+
+            transactionTime.setHours(0,0,0,0);
+            let findstr = "time >= \"" +transactionTime+ "\" and cust_id == \"" +customerId+ "\"";
+            if (sellerid) {
+                findstr += " and seller_id == \"" +sellerid+ "\"";
+            }
+            
+            return table.select().execute(results => {
+                // console.log("User Secret data: " + JSON.stringify(data, null, 4));
+                // console.log("Passbook getTodayTransaction: " + JSON.stringify(results, null, 4));
+                data.push(results);
+            }, meta => {
+                headers = meta;
+            });
+        }).then(() => {
+            let rawresult = CommonHepler.constructResults(headers, data);
+            let results = [];
+
+            for(let index in rawresult) {
+                let result = HelperFn.transactionTable.fromDBtoParam(rawresult[index]);
+                results.push(result);
+            }
+
+            // console.log("Passbook getTodayTransaction result: " +JSON.stringify(results));
+            resolve(results);
+        }).catch((err) => {
+            // console.error("Passbook getTodayTransaction err: " +JSON.stringify(err));
+            if(!err.info) {
+                err.info = {};
+                err.info.code = "FF";
+            }
+            reject(err);
+        }).finally(() => {
+            if (connection)
+                connection.close();
+        });
+    });
 }
